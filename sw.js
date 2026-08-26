8208d467c4e213d2d1250f142cedb2ce468f87bd
fajarsidikrmd <hackermail492@gmail.com>
2026-08-26 12:50:54 +0700

    Add service worker for caching static assets

---
+++ sw.js
@@ -0,0 +1,30 @@
+const CACHE_NAME = 'gas-studio-v2';
+const STATIC_ASSETS = [
+  './',
+  './index.html',
+  './manifest.json'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(STATIC_ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((cachedResponse) => {
+      return cachedResponse || fetch(e.request).catch(() => caches.match('./index.html'));
+    })
+  );
+});
